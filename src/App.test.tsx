23c3ed205a97ff43e5f0995bd006cd4c7c6c248e
Joe Burton <joeburton@gmail.com>
2024-05-23 10:11:52 +0100

    chore: updated test desc

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,13 +1,13 @@
-import { ApolloProvider } from '@apollo/client';
-import { render } from '@testing-library/react';
-import client from './apollo/apolloClient';
-import App from './App';
+import { ApolloProvider } from "@apollo/client";
+import { render, screen } from "@testing-library/react";
+import client from "./apollo/apolloClient";
+import App from "./App";
 
-test('renders learn react link', () => {
-  const { getByTestId } = render(
+test("should renders todo application", () => {
+  render(
     <ApolloProvider client={client}>
       <App />
     </ApolloProvider>
   );
-  expect(getByTestId('simple-todo-app')).toBeInTheDocument();
+  expect(screen.getByTestId("simple-todo-app")).toBeInTheDocument();
 });
